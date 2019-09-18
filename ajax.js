fd980ef62966be90982fd8f35fbb26e379c1e369
josed <metalbox9000@gmail.com>
2019-09-18 13:45:29 -0300

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,4 +1,5 @@
-   var idsr; 	
+   	
+var idsr; 	
 	 idsr=1; // any value, for example to consult in DB
 	
 	 
@@ -11,12 +12,12 @@
 	}
 	
 	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) 
-		{		
-        // convert json Response from PHP File to Array
-				var data = JSON.parse(this.responseText);									
-				
-         alert(data['this is my title: '+ data["title"] + 'and my number:' + data["id"]  );		
+	if (this.readyState == 4 && this.status == 200) 
+	{		
+		// convert json Response from PHP File to Array
+		var data = JSON.parse(this.responseText);									
+
+		alert('this is my title: '+ data["title"] + ' and my number:' + data["id"]  );		
 		
 	};
 	xmlhttp.open("GET","php_response.php?idsr="+idsr,true);
