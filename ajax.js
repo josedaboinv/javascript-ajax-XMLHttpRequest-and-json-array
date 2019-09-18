cb6cb64f28054ada91746f43a248e3e68c9092c1
josed <metalbox9000@gmail.com>
2019-09-18 13:43:53 -0300

    Create ajax.js

---
+++ ajax.js
@@ -0,0 +1,23 @@
+   var idsr; 	
+	 idsr=1; // any value, for example to consult in DB
+	
+	 
+	if (window.XMLHttpRequest) {
+		// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp = new XMLHttpRequest();
+	} else {
+		// code for IE6, IE5
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) 
+		{		
+        // convert json Response from PHP File to Array
+				var data = JSON.parse(this.responseText);									
+				
+         alert(data['this is my title: '+ data["title"] + 'and my number:' + data["id"]  );		
+		
+	};
+	xmlhttp.open("GET","php_response.php?idsr="+idsr,true);
+	xmlhttp.send();			
